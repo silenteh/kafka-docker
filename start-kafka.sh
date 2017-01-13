6d0faebb0dd25afb0333a4f69f451654144bec2e
silenteh <silenteh@gmail.com>
2017-01-13 18:54:14 +0100

    Added default for kafka jmx port

--- start-kafka.sh
+++ start-kafka.sh
@@ -3,6 +3,9 @@
 if [[ -z "$KAFKA_PORT" ]]; then
     export KAFKA_PORT=9092
 fi
+if [[ -z "$KAFKA_JMX_PORT" ]]; then
+    export KAFKA_JMX_PORT=9998
+fi
 if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
     export KAFKA_ADVERTISED_PORT=$(docker port `hostname` $KAFKA_PORT | sed -r "s/.*:(.*)/\1/g")
 fi
